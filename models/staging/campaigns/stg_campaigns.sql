67dab1d891ec90464a96e4d24ca5e17242fee056
Miles Freeborn <mfreeborn@dbtlabs.com>
2025-12-02 10:36:03 -0500

    Add models, seeds, and dependencies from mesh_marketing project
    - Added models directory with staging, intermediate, and marts layers
    - Added campaigns.csv and user_engagement.csv seed files
    - Added dependencies.yml with data_engineering and da_sales projects
    - Updated cross-project references from analytics to data_engineering

---
+++ models/staging/campaigns/stg_campaigns.sql
@@ -0,0 +1,23 @@
+
+
+
+with campaigns as (
+
+    select * from {{ ref('campaigns') }}
+
+),final as (
+
+    select
+    
+    id,
+    seq,
+    name,
+    tier_name,
+    owner
+
+    from campaigns
+
+)
+
+select * from final
+
---
+++ models/staging/website/stg_engaged_users.sql
@@ -0,0 +1,2 @@
+select * from {{ ref('user_engagement') }}
+
