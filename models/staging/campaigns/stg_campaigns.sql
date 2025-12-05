cc80c72536e1f1a739a5b1b1691aa9ac11c338fb
Miles Freeborn <miles.freeborn@dbtlabs.com>
2025-12-05 12:18:58 +0000

    linted

--- models/staging/campaigns/stg_campaigns.sql
+++ models/staging/campaigns/stg_campaigns.sql
@@ -1,23 +1,21 @@
-
-
-
 with campaigns as (
 
     select * from {{ ref('campaigns') }}
 
-),final as (
+),
+
+final as (
 
     select
-    
-    id,
-    seq,
-    name,
-    tier_name,
-    owner
+
+        id,
+        seq,
+        name,
+        tier_name,
+        owner
 
     from campaigns
 
 )
 
 select * from final
-
--- models/staging/website/stg_engaged_users.sql
+++ models/staging/website/stg_engaged_users.sql
@@ -1,2 +1,1 @@
 select * from {{ ref('user_engagement') }}
-
